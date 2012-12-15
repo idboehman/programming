19d2170cbadc77af8ff8f4e6f8f29be6a68d7cd6
Isaac Boehman <idboehman+github@gmail.com>
2012-12-15 15:56:54 -0500

    Changed script to take in user input instead of hardcoding IP & port

--- python/violent/network_basics.py
+++ python/violent/network_basics.py
@@ -4,12 +4,16 @@
 """
 import socket
 # imports the socket module used for connecting to specified IP addresses
-socket.setdefaulttimeout(2) # set's the default timeout to 2 seconds
+socket.setdefaulttimeout(10) # set's the default timeout to 2 seconds
 sock = socket.socket() # creates a new socket called sock
-sock.connect(("10.235.1.6",22)) #connects to IP specified on port 22
+print 'What host would you like to grab banners from?'
+ip = raw_input('> ')
+print 'What port would you like to conncet to?'
+port = raw_input('> ')
+sock.connect((ip,int(port))) #connects to IP specified on the port specified
 # in this case, my Debian server
 banner = sock.recv(1024)
 # grabs the banner from the connection, the first 1024 bytes
 # of the connection
-print banner
+print 'Banner grabbed: ', banner
 
