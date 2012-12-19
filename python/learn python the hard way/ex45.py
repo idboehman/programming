b6f26447f632a4b83a7bf8761cb8ec89b1970863
Isaac Boehman <idboehman+github@gmail.com>
2012-12-18 23:08:54 -0500

    Rewrote function names to comply with Python style guide

--- python/learn python the hard way/ex45.py
+++ python/learn python the hard way/ex45.py
@@ -7,20 +7,20 @@ from random import randint # used for random number generation
 
 class Board(object):
     
-    def initBoard(self):
+    def initboard(self):
         """Creates a 5x5 game board"""
         board = []
         for x in range(0,5):
             board.append(['O'] * 5)
         return board
 
-    def printBoard(self, board):
+    def printboard(self, board):
         """Prints the board in a more visually appealing manner"""
         for row in board:
             print ' '.join(row)
         print "\n"
 
-    def inRange(self, board, guess_row, guess_col):
+    def inrange(self, board, guess_row, guess_col):
         if (guess_row < 1 or guess_col < 1 or
             guess_row > len(board) or 
             guess_col > len(board[0])):
@@ -28,27 +28,18 @@ class Board(object):
         else:
             return True
  
-    def sameGuess(self, board, guess_row, guess_col):
+    def sameguess(self, board, guess_row, guess_col):
         if board[guess_row - 1][guess_col - 1] == "X":
            return True 
         
 
 class Ship(object):
-#row =
-    ''' 
-    def __init__(self, row=None, col=None, board):
-        """Generates a random spot on the board to place the ship"""
-        self.row = row if row is not None else self.randomRow(board)
-        self.col = col if col is not None else self.randomCol(board)
-        #self.row = self.random_row(Board.board())
-        #self.col = self.random_col(Board.board())
-    ''' 
 
-    def randomRow(self, board):
+    def random_row(self, board):
         """Returns a random row on the board"""
         # Number between 0 and len of board - 1 (b/c arrays start at 0)
         return randint(0, len(board) - 1)
-    def randomCol(self, board):
+    def random_col(self, board):
         """Returns a random column on the board"""
         # since a board is a list of rows, generating a random num
         # between 0 and the len of board[0] - 1 generates a number
@@ -56,10 +47,10 @@ class Ship(object):
         return randint(0, len(board[0]) - 1)
 
 class Game(object):
-    board = Board().initBoard()
+    board = Board().initboard()
     ship = Ship()
-    ship.row = ship.randomRow(board)
-    ship.col = ship.randomCol(board)
+    ship.row = ship.random_row(board)
+    ship.col = ship.random_col(board)
 
     def __init__(self, turns=None):
         # game board to play on, a Game has-a board
@@ -73,7 +64,7 @@ class Game(object):
         #print self.ship.row
         #print self.ship.col
         #print "\n"
-        Board().printBoard(self.board)
+        Board().printboard(self.board)
         print "You have " + str(self.turns) + " guesses"
         print self.ship.row, self.ship.col
 
@@ -88,21 +79,21 @@ class Game(object):
                 print 'Congratulations! You sunk my battleship!'
                 print 'You win!'
                 self.board[guess_row - 1][guess_col - 1] = "&"
-                Board().printBoard(self.board)
+                Board().printboard(self.board)
                 break
-            elif not (Board().inRange(self.board, guess_row, guess_col)):
+            elif not (Board().inrange(self.board, guess_row, guess_col)):
                 print "Oops, that's not even in the ocean!",
                 print "\nPlease try again, this time guessing",
                 print "between 1 and 5"
                 print "You have " + left + " turns remaining\n"
-            elif Board().sameGuess(self.board, guess_row, guess_col):
+            elif Board().sameguess(self.board, guess_row, guess_col):
                 print "You've guessed that already silly!"
                 print "You have " + left + " turns remaining\n"
             else:
                 print 'You missed my battleship!'
                 self.board[guess_row - 1][guess_col - 1] = "X"
                 print "You have " + left + " turns remaining\n"
-            Board().printBoard(self.board)
+            Board().printboard(self.board)
         else:
             print "Out of guesses, you lose!"
 
